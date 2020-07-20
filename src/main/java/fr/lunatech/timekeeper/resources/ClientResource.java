ae3961cb8d935b957e8a3ea3f8f40a8461cb1e8a
thierry kuy <kuy.thierry@gmail.com>
2020-07-20 11:28:15 +0200

    Metrics on ClientResource

--- src/main/java/fr/lunatech/timekeeper/resources/ClientResource.java
+++ src/main/java/fr/lunatech/timekeeper/resources/ClientResource.java
@@ -5,6 +5,10 @@ import fr.lunatech.timekeeper.resources.providers.AuthenticationContextProvider;
 import fr.lunatech.timekeeper.services.ClientService;
 import fr.lunatech.timekeeper.services.requests.ClientRequest;
 import fr.lunatech.timekeeper.services.responses.ClientResponse;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
@@ -25,6 +29,8 @@ public class ClientResource implements ClientResourceApi {
 
     @RolesAllowed({"user", "admin"})
     @Override
+    @Counted(name = "countGetAllClients", description = "Counts how many times the user load the client list on method 'getAllClients'")
+    @Timed(name = "timeGetAllClients", description = "Times how long it takes the user load the client list on method 'getAllClients'", unit = MetricUnits.MILLISECONDS)
     public List<ClientResponse> getAllClients() {
         final var ctx = authentication.context();
         return clientService.listAllResponses(ctx);
@@ -32,6 +38,8 @@ public class ClientResource implements ClientResourceApi {
 
     @RolesAllowed({"admin"})
     @Override
+    @Counted(name = "countCreateClient", description = "Counts how many times the user to create the client on method 'createClient'")
+    @Timed(name = "timeCreateClient", description = "Times how long it takes the user to create the client on method 'createClient'", unit = MetricUnits.MILLISECONDS)
     public Response createClient(@Valid ClientRequest request, UriInfo uriInfo) {
         final var ctx = authentication.context();
         final long clientId = clientService.create(request, ctx);
@@ -39,6 +47,8 @@ public class ClientResource implements ClientResourceApi {
         return Response.created(uri).build();
     }
 
+    @Counted(name = "countGetClient", description = "Counts how many times the user to get the client on method 'getClient'")
+    @Timed(name = "timeGetClient", description = "Times how long it takes the user to get the client on method 'getClient'", unit = MetricUnits.MILLISECONDS)
     @RolesAllowed({"user", "admin"})
     @Override
     public ClientResponse getClient(Long id) {
@@ -49,6 +59,8 @@ public class ClientResource implements ClientResourceApi {
 
     @RolesAllowed({"admin"})
     @Override
+    @Counted(name = "countUpdateClient", description = "Counts how many times the user to update the client on method 'updateClient'")
+    @Timed(name = "timeUpdateClient", description = "Times how long it takes the user to update the client on method 'updateClient'", unit = MetricUnits.MILLISECONDS)
     public Response updateClient(Long id, @Valid ClientRequest request) {
         final var ctx = authentication.context();
         return clientService.update(id, request, ctx)
